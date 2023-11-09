8d16149e54789a2d8da7d0e5efcc4bc2678af626
AaronS <aphilvs@gmail.com>
2023-11-09 14:59:41 +0800

    Add shebang and logging to setup.sh and update Procfile

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+echo "Setting up Streamlit configuration"
+
 mkdir -p ~/.streamlit/
 
 echo "\
@@ -5,9 +9,13 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
+echo "Generated credentials.toml file"
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+echo "Generated config.toml file with port $PORT"
